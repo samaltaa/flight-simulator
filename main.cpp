0d6c2d4725687680d7ca7e684a4b27c533dffd51
Samira <samirasanchez8090@gmail.com>
2023-09-07 16:02:40 -0400

    main.cpp boiler plate

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/*
+from past samira to future Samira:
+    
+*/
+
+
 #include "geometry.h"
 //#include "ship.h" //TODO: finish ship header file
 //#include "landscape.h" //TODO: finish landscape header file 
@@ -9,3 +15,28 @@
 #endif
 #include <iostream>
 
+int main(int argc, char** argv) {
+    // Initialize GLUT and create a window
+
+    // Create instances of the ship and landscape classes
+    //Ship ship; TODO: once the ship header file is done uncomment this
+    //Landscape landscape; TODO: same as above but for landscape
+    
+    // Initialize other necessary components and settings
+
+    // Main simulation loop
+    while (true) {
+        // Handle user input (e.g., keyboard and mouse events)
+
+        // Update the ship's state (e.g., position, orientation)
+
+        // Update the landscape (e.g., terrain, obstacles)
+
+        // Render the scene (using OpenGL)
+
+        // Swap buffers and handle other GLUT events
+    }
+
+
+    return 0;
+}
