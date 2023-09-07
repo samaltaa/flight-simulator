8d90dfc76d7c634d96e02d61804f599c7f408608
Samira <samirasanchez8090@gmail.com>
2023-09-07 16:00:03 -0400

    main entry point created and modules imported

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "geometry.h"
+//#include "ship.h" //TODO: finish ship header file
+//#include "landscape.h" //TODO: finish landscape header file 
+//#include "cockpit.h" // TODO: finish cockpit header dile
+#ifdef __APPLE_CC__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include <iostream>
+
