0f089ef21d349937f0fa176e14af5a3e66b2126c
Samira <samirasanchez8090@gmail.com>
2023-09-11 19:10:35 -0400

    landscape class defined

--- landscape.h
+++ landscape.h
@@ -12,3 +12,39 @@
 //                      display lists for efficient drawing.
 // draw()               draws the landscape using a fixed coloring scheme.
 // drawWireFrame()      draws a wireframe representation of the landscape.
+
+#ifndef LANDSCAPE_H_
+#define LANDSCAPE_H_
+
+#ifdef __APPLE_CC__
+#include <GLUT/glut.h>
+#else
+#include <GLUT/glut.h>
+#endif
+
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+
+class Landscape {
+  int rows;
+  int columns;
+  double highest;                         // the highest point in the mesh
+  std::vector< std::vector<double>> d;  // the grid of elevations
+  int solidId;                            // display list id for solid mesh
+  int wireFrameId;                        // display list id for wire mesh
+  static double unused;
+  void generate(int x1, int y1, int x2, int y2, double rug);
+  double scale(double x) {return x * (((double)rand()/RAND_MAX) - 0.5);}
+  void drawTriangle(int x1, int z1, int x2, int z2, int x3, int z3);
+  void vertex(double x, double z);
+  void createSolidDisplayList();
+  void createWireFrameDisplayList();
+public:
+  Landscape(int rows, int columns);
+  void create(double rug);
+  void draw() {glCallList(solidId);}
+  void drawWireFrame() {glCallList(wireFrameId);}
+};
+
+#endif
